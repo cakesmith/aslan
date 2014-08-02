78c5d7254ea3a9bcdccbb8fdb323d716184b398f
Nick LaRosa <nick@larosa.com>
2014-08-02 02:59:23 -0400

    add carousel

--- src/app/app.js
+++ src/app/app.js
@@ -17,6 +17,47 @@
 
   aslan.controller('AppCtrl', ['$scope', '$window', function($scope, $window) {
 
+      $scope.myInterval = 5000;
+      var slides = $scope.slides = [];
+
+    var images = [
+      {
+        label: 'Commercial Lighting',
+        img: '6571'
+      },
+      {
+        label: 'Home Automation',
+        img: '6682'
+      },
+      {
+        label: 'Design Engineering',
+        img: '6683'
+      },
+      {
+        label: 'Network Design',
+        img: '6695'
+      },
+      {
+        label: 'Municipal Services',
+        img: '7152'
+      },
+      {
+        label: 'Retrofit Upgrades',
+        img: '7211'
+      }
+    ];
+
+    var base = 'http://gdriv.es/aslanelectric/img_';
+
+    angular.forEach(images, function(value) {
+      slides.push({
+        image: base + value.img + '.jpg',
+        text: value.label
+      });
+    });
+
+
+
     $scope.nav = $scope.dropDown= {};
 
     $scope.nav.isCollapsed = true;
