f7c240e58380a31fd98a7d7ccd2b0c0595e78ee4
Nick LaRosa <nick@larosa.com>
2014-08-02 18:23:11 -0400

    version bump to 0.2.3 refactoring images to a service

--- src/app/app.js
+++ src/app/app.js
@@ -1,9 +1,9 @@
-(function(aslan) {
+(function (aslan) {
   'use strict';
 
   aslan.value('duScrollOffset', 49);
 
-  aslan.controller('Services', ['$scope', function($scope) {
+  aslan.controller('Services', ['$scope', function ($scope) {
 
     $scope.services = [
       {
@@ -18,47 +18,10 @@
 
   }]);
 
-  aslan.controller('AppCtrl', ['$scope', '$window', function($scope, $window) {
+  aslan.controller('AppCtrl', ['$scope', '$window', 'imageService', function ($scope, $window, imageService) {
 
-      $scope.myInterval = 5000;
-      var slides = $scope.slides = [];
-
-    var images = [
-      {
-        label: 'Commercial Lighting',
-        img: '6571'
-      },
-      {
-        label: 'Home Automation',
-        img: '6682'
-      },
-      {
-        label: 'Design Engineering',
-        img: '6683'
-      },
-      {
-        label: 'Network Design',
-        img: '6695'
-      },
-      {
-        label: 'Municipal Services',
-        img: '7152'
-      },
-      {
-        label: 'Retrofit Upgrades',
-        img: '7211'
-      }
-    ];
-
-    var base = 'http://gdriv.es/aslanelectric/img_';
-//    var base = 'http://gdriv.es/aslanelectric/_';
-
-    angular.forEach(images, function(value) {
-      slides.push({
-        image: base + value.img + '.jpg',
-        text: value.label
-      });
-    });
+    $scope.myInterval = 5000;
+    $scope.slides = imageService.slides;
 
     var nav, dropDown;
 
@@ -72,14 +35,14 @@
       $scope.$apply();
     });
 
-    $scope.navClick = function($event) {
+    $scope.navClick = function ($event) {
       $event.preventDefault();
       $event.stopPropagation();
       nav.isCollapsed = true;
       dropDown.isOpen = false;
     };
 
-    $scope.navToggle = function() {
+    $scope.navToggle = function () {
       nav.isCollapsed = !nav.isCollapsed;
     };
 
@@ -87,6 +50,7 @@
 
 
 }(angular.module('aslan', [
+  'aslan.image',
   'aslan.loader',
   'ngRoute',
   'ui.bootstrap',
---
+++ src/app/services/image/image.js
@@ -0,0 +1,62 @@
+(function (image) {
+
+  image.factory('imageService', function () {
+
+    var service = {
+
+      base: 'http://gdriv.es/aslanelectric/',
+      prefix: 'img_',
+      suffix: '.jpg',
+      slides: [],
+      images: [
+        {
+          text: 'Commercial Lighting',
+          id  : '6571',
+          type: 'slide'
+        },
+        {
+          text: 'Home Automation',
+          id  : '6682',
+          type: 'slide'
+        },
+        {
+          text: 'Design Engineering',
+          id  : '6683',
+          type: 'slide'
+        },
+        {
+          text: 'Network Design',
+          id  : '6695',
+          type: 'slide'
+        },
+        {
+          text: 'Municipal Services',
+          id  : '7152',
+          type: 'slide'
+        },
+        {
+          text: 'Retrofit Upgrades',
+          id  : '7211',
+          type: 'slide'
+        }
+      ]
+
+    };
+
+
+    angular.forEach(service.images, function (value) {
+      value.image = service.base + service.prefix + value.id + service.suffix;
+      if (value.type === 'slide') {
+        service.slides.push(value);
+      }
+
+    });
+
+
+
+    return service;
+
+  });
+
+
+}(angular.module('aslan.image', [])));
